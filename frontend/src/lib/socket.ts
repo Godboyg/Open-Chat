319c06f4fd7202e0bd7ad11880a8ce284f028268
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2026-01-10 09:58:26 +0530

    Update socket.ts

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -3,7 +3,7 @@ let socket: WebSocket | null = null
 export function getSocket() {
 
   if (!socket || socket.readyState === WebSocket.CLOSED) {
-    socket = new WebSocket('https://open-chat-v9i4.onrender.com')
+    socket = new WebSocket('wss://open-chat-v9i4.onrender.com')
 
     socket.onopen = () => {
       console.log('✅ Connected to WebSocket server')
