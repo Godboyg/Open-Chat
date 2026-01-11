fdec5b6cf30664ae94df6fc6ae1a9c4977842ae7
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2026-01-11 20:00:11 +0530

    Update notification.ts

--- backend/src/models/notification.ts
+++ backend/src/models/notification.ts
@@ -11,6 +11,14 @@ const notificationSchema = new mongoose.Schema({
     createdAt: { type: Date , default: Date.now}
 })
 
+notificationSchema.index(
+  { userId: 1, from: 1, to: 1, type: 1 },
+  {
+    unique: true,
+    partialFilterExpression: { type: "FRIEND_REQUEST" }
+  }
+);
+
 export type notificationType = InferSchemaType<typeof notificationSchema>
 
 const notification:Model<notificationType> = mongoose.models.notification || mongoose.model("notification", notificationSchema)
