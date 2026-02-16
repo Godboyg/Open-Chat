6fc6d5ac50f57e99e493a6c3fc1cc11e4d386c42
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2026-02-16 07:16:29 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,29 +1,30 @@
-"use client"
 import "./globals.css";
 import ThemeProvider from "./Components/ThemeProvider";
 import Script from "next/script";
 
+export const metadata = {
+  other: {
+    "google-adsense-account": "ca-pub-2915175777820694",
+  },
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-
+}) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-2915175777820694" />
-      </head>
-      <body
-        className=""
-      >
-        <ThemeProvider>
-          <Script
+      <body>
+        <Script
+          id="adsense-script"
           async
           strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
           crossOrigin="anonymous"
         />
+
+        <ThemeProvider>
           {children}
         </ThemeProvider>
       </body>
