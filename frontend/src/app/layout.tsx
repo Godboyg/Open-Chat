48dd5378d865cc3f71780435e774fecfcb2bed78
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2026-02-16 06:36:29 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,6 +10,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
+        crossorigin="anonymous"></script>
+      </head>
       <body
         className=""
       >
