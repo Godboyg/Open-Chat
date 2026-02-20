04769341c5f3ed041eb6d88120acbb38ea277dba
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2026-02-20 08:38:05 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ import { messageRouter } from './routes/messages.route.js'
 import { conversationRouter } from './routes/conversation.route.js'
 
 export const app = new Elysia({ adapter: node() })
-    .use(cors({ origin: "http://localhost:3000" }))
+    .use(cors({ origin: "https://open-chat-roan.vercel.app" }))
     .use(userRoute)
     .use(notificationRoute)
     .use(conversationRouter)
